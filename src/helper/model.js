75e533cd4301524ec0e654f88a1a2ace723674e9
duke7able <mehtashreyan041.sm@gmail.com>
2019-01-08 01:29:45 +0530

    localStorage replaced by persistentStorage

--- src/helper/model.js
+++ src/helper/model.js
@@ -1,6 +1,9 @@
 /*
  * Helper methods not so react way but just providing abstraction
  */
+
+import PersistentStore from 'persistent-storage';
+import StorageShim from 'node-storage-shim';
 /*
  *  freezing the object to make it immutable
  *  also its better and faster to handle the booleans that are coming as string
@@ -11,13 +14,19 @@ export const BOOLS = Object.freeze({
   false: false
 });
 
+const STORE = new PersistentStore({
+  useCompression: false, 
+  storageBackend: new StorageShim()
+})
+
 export function setLogin(isLogin) {
-  localStorage.setItem("UserLoggedIn", isLogin);
+  STORE.setItem("UserLoggedIn",isLogin);
+  // localStorage.setItem("UserLoggedIn", isLogin);
   return true;
 }
 
 export function isUserLoggedIn() {
-  return BOOLS[localStorage.getItem("UserLoggedIn")];
+  return BOOLS[STORE.getItem("UserLoggedIn")];
 }
 
 /*
